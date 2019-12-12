8746abf8411ecc284c9d3be2739bbdda47a9b9b5
Steven-Cadena <steven@gmail.com>
2019-12-12 19:40:01 +0100

    añadido al menu anuncio y nuevo anuncio

--- resources/templates/navegacion.php
+++ resources/templates/navegacion.php
@@ -2,4 +2,6 @@
   <a href="actividades.php">actividades</a>
   <a href="amigos.php">amigos</a>
   <a href="registro.php">registro</a>
+  <a href="anuncio.php">anuncio</a>
+  <a href="anuncioNuevo.php">Anuncio NUEVO</a>
 </nav>
