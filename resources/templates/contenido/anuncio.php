52e7e9efefe880fcf8b1c4fa2b5c22f604f82f99
Steven-Cadena <steven@gmail.com>
2019-12-12 19:35:52 +0100

    añadida sesion en anuncios.php

--- resources/templates/contenido/anuncio.php
+++ resources/templates/contenido/anuncio.php
@@ -1,6 +1,12 @@
 <?php
 
-$id=1;
+session_start();
+  echo $_SESSION['id'].' Las id  es <br>';
+if( !isset($_SESSION['id']) ){
+    header('Location: login.php');
+    die();
+}
+$id=$_SESSION['id'];
 
 $resultados = AnuncioManager::obtenerAnuncioPorIdCliente($id);
 
