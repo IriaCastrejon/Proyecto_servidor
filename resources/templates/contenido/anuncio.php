d2973d7a1b4ba0f8ab6eb42d783a1eb85d9f52f5
mayradmt96 <56133118+mayradmt96@users.noreply.github.com>
2019-12-16 16:29:31 +0100

    Update anuncio.php

--- resources/templates/contenido/anuncio.php
+++ resources/templates/contenido/anuncio.php
@@ -6,15 +6,33 @@ if( !isset($_SESSION['id']) ){
     header('Location: login.php');
     die();
 }
+
 $id=$_SESSION['id'];
 
 $resultados = AnuncioManager::obtenerAnuncioPorIdCliente($id);
 
-foreach ($resultados as $fila) { ?>
-     <div class="anuncios">
-       <h4><?=$fila->getFechaAlta()?></h4>
-       <h4><?=$fila->getUrl()?></h4>
-       <img src="<?=$fila->getFoto()?>" alt="">
-     </div>
+function dias_pasados($fecha_inicial,$fecha_final){
+  $dias = (strtotime($fecha_inicial)-strtotime($fecha_final))/86400;
+  $dias = abs($dias); $dias = floor($dias);
+  return $dias;
+}
 
-<?php } ?>
+//echo dias_pasados($fecha_baja,$fecha_alta);
+$precioDia = 25;
+
+?>
+
+
+<div class="contenedor_anuncios">
+  <h1> Mis anuncios </h1>
+  <?php foreach ($resultados as $fila) { ?>
+       <div class="anuncios">
+         <h5> Fecha alta <br><?=substr($fila->getFechaAlta(),0,10) ?></h5>
+         <h5> Fecha baja <br><?=$fila->getFechaBaja()?></h5>
+         <h5> Precio <br><?php echo dias_pasados($fila->getFechaBaja(),$fila->getFechaAlta()) * $precioDia . ' €'?></h5>
+         <h5><?=$fila->getFoto()?></h5>
+         <img src="../imgs/InicioFondo1.png" alt="">
+       </div>
+
+  <?php } ?>
+</div>
